ead1716bdfd810e6ff5501a23be0805458c3496e
FSD_ayush353 <a27392368@gmail.com>
2026-07-29 15:33:31 +0530

    table

---
+++ 1.js
@@ -0,0 +1,9 @@
+const students = {
+    name: "ayush shukla",
+    age: 34,
+    branch: "cse"
+};
+const{name,age,branch}=students;
+console.log("name=",name);
+console.log("age=",age);
+console.log("branch=",branch);

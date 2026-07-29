18ea923fa7b2874465608a97beeac7f183981100
FSD_ayush353 <a27392368@gmail.com>
2026-07-29 15:22:10 +0530

    arr

---
+++ arr.js
@@ -0,0 +1,8 @@
+const num=[1,2,3,4,5];
+const even=num.filter((i)=>(i%2==0));
+console.log("nums",num);
+console.log("nums",even);
+const sqr=even.map((i)=>(i*i));
+console.log(sqr);
+const sum=square.reduce((i,s)=>(i+s));
+console.log("sum",sum);

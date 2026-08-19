afd57944863be58dc6cd2bbf708075b2d2225db6
FSD_ayush353 <a27392368@gmail.com>
2026-08-19 15:36:03 +0530

    bookstore

--- html dom/bookstore.js
+++ html dom/bookstore.js
@@ -1,4 +1,4 @@
-const books = [
+ const books = [
     {
         name: "The Alchemist",
         price: "₹425",
@@ -15,21 +15,44 @@ const books = [
         image: "https://images.unsplash.com/photo-1512820790803-83ca734da794"
     }
 ];
+
 const parent = document.getElementById("bookstore");
+
 books.forEach(function(book) {
+
     const div = document.createElement("div");
+
     div.setAttribute("class", "book");
+
     const image = document.createElement("img");
+
     image.setAttribute("src", book.image);
+
     const h2 = document.createElement("h2");
+
     h2.innerText = book.name;
+
     const price = document.createElement("h2");
+
     price.innerText = "Price: " + book.price;
+
     const bt = document.createElement("button");
+
     bt.innerText = "Add to Cart";
+
+    bt.onclick = function() {
+        addtocart(book.name);
+    };
+
     div.appendChild(image);
     div.appendChild(h2);
     div.appendChild(price);
     div.appendChild(bt);
+
     parent.appendChild(div);
 });
+
+
+function addtocart(data) {
+    alert(data + " added to cart");
+}
